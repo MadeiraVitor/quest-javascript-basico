460d676795f81a70c6e20e8ce246fbcd268dc0af
MadeiraVitor <vitorsoutom@hotmail.com>
2022-03-25 13:41:26 -0300

    adiciona a quest de JavaScript Básico

---
+++ index.js
@@ -0,0 +1,27 @@
+function somar(numero1, numero2){
+    const resultadoDaSoma = numero1 + numero2
+    console.log("A soma dos dois números é: " + resultadoDaSoma)
+}
+
+somar(5, 5)
+
+function dividir(numero1, numero2){
+    const resultadoDaDivisao = numero1 / numero2
+    console.log("A divisão dos dois números é: " + resultadoDaDivisao)
+}
+
+dividir(10, 2)
+
+function multiplicar(numero1, numero2){
+    const resultadoDaMultiplicacao = numero1 * numero2
+    console.log("A multiplicação dos dois números é: " + resultadoDaMultiplicacao)
+}
+
+multiplicar(10, 2)
+
+function subtrair(numero1, numero2){
+    const resultadoDaSubtracao = numero1 - numero2
+    console.log("A subtração dos dois números é: " + resultadoDaSubtracao)
+}
+
+subtrair(10, 2)
